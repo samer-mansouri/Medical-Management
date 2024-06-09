169e3a3a3b967e5431d28e13f66b0086e8e34230
samer-mansouri <samer.mansouri@insat.ucar.tn>
2024-06-09 19:11:39 +0100

    dov dov

--- app.js
+++ app.js
@@ -824,6 +824,67 @@ app.get('/logind', (req, res) => {
 	res.render('logind');
 });
 app.get('/dashbord', (req, res) => {
+    // count for those tables patient , docteur , consultation , rendezvous , ordonnace
+
+    const count = {
+        patient: 0,
+        docteur: 0,
+        consultation: 0,
+        rendezvous: 0,
+        ordonnace: 0,
+    };
+    db.query('SELECT COUNT(*) AS patient FROM patient', (err, results) => {
+      if (err) {
+          console.error('Error fetching job details from database:', err);
+          res.status(500).send('Error fetching job details');
+          return;
+      }
+      count.patient = results[0].patient;
+  });
+
+
+    db.query('SELECT COUNT(*) AS docteur FROM docteur', (err, results) => {
+      if (err) {
+          console.error('Error fetching job details from database:', err);
+          res.status(500).send('Error fetching job details');
+          return;
+      }
+      count.docteur = results[0].docteur;
+  });
+
+  db.query('SELECT COUNT(*) AS consultation FROM consultation', (err, results) => {
+      if (err) {
+          console.error('Error fetching job details from database:', err);
+          res.status(500).send('Error fetching job details');
+          return;
+      }
+      count.consultation = results[0].consultation;
+  });
+
+  db.query('SELECT COUNT(*) AS rendezvous FROM rendezvous', (err, results) => {
+      if (err) {
+          console.error('Error fetching job details from database:', err);
+          res.status(500).send('Error fetching job details');
+          return;
+      }
+      count.rendezvous = results[0].rendezvous;
+  });
+
+  db.query('SELECT COUNT(*) AS ordonnace FROM ordonnace', (err, results) => {
+      if (err) {
+          console.error('Error fetching job details from database:', err);
+          res.status(500).send('Error fetching job details');
+          return;
+      }
+      count.ordonnace = results[0].ordonnace;
+
+      // Now you can use the count object with the updated counts
+      console.log(count);
+  });
+
+
+
+
     db.query('SELECT * FROM docteur  ',  (err, results) => {
         if (err) {
           console.error('Error fetching job details from database:', err);
